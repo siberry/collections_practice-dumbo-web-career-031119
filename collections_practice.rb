91b0f29b1383840f5c119cf9a4b5f54762ca71aa
Serena Berry <siberry226@gmail.com>
2019-03-17 16:05:03 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,18 @@
+def sort_array_asc(arr)
+  arr.sort
+end
 
+def sort_array_desc(arr)
+  arr.sort.reverse
+end
+
+def sort_array_char_count(arr)
+  arr.sort_by(&:length)
+end
+
+def swap_elements(arr)
+  second = arr[1]
+  arr[1] = arr[2]
+  arr[2] = second
+  arr
+end
